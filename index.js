e3fc2508de7c2e4ac69c86df29ad33d376348045
abhaykawle2010-2010 <aabhhayy2010@gmail.com>
2024-06-07 17:39:19 +0530

    done

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express=require("express");
+const express = require("express");
 const { dbConnection } = require("./config/user.config");
-const cors=require("cors");
+const cors = require("cors");
 const { userRoutes } = require("./routes/users.route");
 const bookRoute = require("./routes/book.route");
 
-app.use(cors());
-const app=express();
+
+const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.use('/pdf',userRoutes);
+app.use('/pdf', userRoutes);
 
-app.use('/books',bookRoute)
+app.use('/books', bookRoute)
 
-app.listen(8080,async()=>{
+app.listen(8080, async () => {
     try {
         await dbConnection;
         console.log("Database is connected");
@@ -27,5 +27,5 @@ app.listen(8080,async()=>{
     } catch (error) {
         console.log(error);
     }
-    
+
 });
